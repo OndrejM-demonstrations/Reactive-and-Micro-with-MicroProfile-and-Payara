801a81eb7d46d05917a2c351252cbc00d216521d
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2018-03-06 12:19:43 +0100

    Add semantic ui and webpack-watch Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- BTCFrontend/src/main/javascript/app/app.js
+++ BTCFrontend/src/main/javascript/app/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'semantic-ui-react'
 
 ReactDOM.render(
-  <h1>Hello, world! This is a test from React!</h1>,
+  <p><Button primary>Click Here. This is a UI thing</Button>Hello, world! This is a test from React!</p>,
   document.getElementById('root')
 );
-
