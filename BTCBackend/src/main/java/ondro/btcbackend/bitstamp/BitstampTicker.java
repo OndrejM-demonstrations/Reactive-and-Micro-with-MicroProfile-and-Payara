6b9c39b6d237ee329be650295041b70449478ab0
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2018-03-09 13:30:33 +0100

    Updated deps from JEE to MP. Added working test to get BTC rate Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

---
+++ BTCBackend/src/main/java/ondro/btcbackend/bitstamp/BitstampTicker.java
@@ -0,0 +1,19 @@
+package ondro.btcbackend.bitstamp;
+
+/**
+ *
+ * @author Ondrej Mihalyi
+ */
+public class BitstampTicker {
+    private Double last;
+
+    public Double getLast() {
+        return last;
+    }
+
+    public void setLast(Double last) {
+        this.last = last;
+    }
+    
+}
+
---
+++ BTCBackend/src/test/java/ondro/btcbackend/BitstampTest.java
@@ -0,0 +1,25 @@
+package ondro.btcbackend;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import ondro.btcbackend.bitstamp.BitstampTicker;
+import static org.assertj.core.api.Assertions.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Ondrej Mihalyi
+ */
+public class BitstampTest {
+
+    @Test
+    public void canRetrieveBtcRate() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("https://www.bitstamp.net/api/ticker/");
+        BitstampTicker ticker = target.request().get(BitstampTicker.class);
+        assertThat(ticker).isNotNull();
+        assertThat(ticker.getLast()).isNotNull();
+    }
+
+}
